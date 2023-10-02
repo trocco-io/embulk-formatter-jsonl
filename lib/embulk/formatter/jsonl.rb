2fd32f85cbbaabac4354afa82c2e5d78ef896c4c
hirose <s1150199@gmail.com>
2023-10-02 18:03:10 +0900

    Revert "embulk-output-sftp で並列実行した際の null エラーのハンドリング"

--- lib/embulk/formatter/jsonl.rb
+++ lib/embulk/formatter/jsonl.rb
@@ -71,15 +71,12 @@ module Embulk
           @schema.each do |col|
             datum[col.name] = @json_columns.include?(col.name) ? JrJackson::Json.load(record[col.index]) : record[col.index]
           end
-
-          data_str = "#{JrJackson::Json.dump(datum, @opts)}#{@newline}".encode(@encoding)
-          @current_file.write data_str
-          @current_file_size += data_str.bytesize
+          @current_file.write "#{JrJackson::Json.dump(datum, @opts )}#{@newline}".encode(@encoding)
         end
       end
 
       def finish
-        file_output.finish unless @current_file.nil?
+        file_output.finish
       end
     end
 
